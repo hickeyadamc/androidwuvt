6ee7d4bb5cdef7839ece25fdd7ccd22ac10b53c8
hickeyadamc <hickeyadamc@gmail.com>
2013-04-30 17:56:35 -0400

    Fixed AsyncHttpUtils and included example usage

--- src/edu/vt/wuvt/androidwuvt/utils/httputils/AsyncHttpUtils.java
+++ src/edu/vt/wuvt/androidwuvt/utils/httputils/AsyncHttpUtils.java
@@ -1,55 +1,77 @@
 package edu.vt.wuvt.androidwuvt.utils.httputils;
 
+import java.io.IOException;
 import java.util.Map;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.ParseException;
+import org.apache.http.util.EntityUtils;
 
 import edu.vt.wuvt.androidwuvt.utils.httputils.GenericAsyncTask.GenericCallback;
 
 public class AsyncHttpUtils {
 	public interface AsyncHttpListener {
-		public void onResponse(HttpResponse response);
+		public void onResponse(String response);
 	}
-	public static void doGet(final String url, final AsyncHttpListener listener) {
-		GenericAsyncTask<HttpResponse> asyncTask = new GenericAsyncTask<HttpResponse>(new GenericCallback<HttpResponse>() {
+	public interface AsyncHttpErrorListener {
+		public void onError(Exception exception);
+	}
+	public static void doGet(final String url, final AsyncHttpListener listener, final AsyncHttpErrorListener errorListener) {
+		GenericAsyncTask<String> asyncTask = new GenericAsyncTask<String>(new GenericCallback<String>() {
 
 			@Override
-			public HttpResponse toCall() {
-				return HttpUtils.doGet(url);
+			public String toCall() {
+				HttpResponse response = HttpUtils.doGet(url);
+				try {
+					return EntityUtils.toString(response.getEntity());
+				} catch (ParseException e) {
+					errorListener.onError(e);
+				} catch (IOException e) {
+					errorListener.onError(e);
+				}
+				return null;
 			}
 
 			@Override
-			public void onComplete(HttpResponse result) {
+			public void onComplete(String result) {
 				listener.onResponse(result);
 				
 			}
 
 			@Override
 			public void onError(Exception error) {
-				// TODO Auto-generated method stub
+				errorListener.onError(error);
 				
 			}
 		});
 		asyncTask.execute();
 		
 	}
-	public static void doGet(final String url, final Map<String, String> queryString, final AsyncHttpListener listener) {
-		GenericAsyncTask<HttpResponse> asyncTask = new GenericAsyncTask<HttpResponse>(new GenericCallback<HttpResponse>() {
+	public static void doGet(final String url, final Map<String, String> queryString, final AsyncHttpListener listener,final AsyncHttpErrorListener errorListener) {
+		GenericAsyncTask<String> asyncTask = new GenericAsyncTask<String>(new GenericCallback<String>() {
 
 			@Override
-			public HttpResponse toCall() {
-				return HttpUtils.doGet(url,queryString);
+			public String toCall() {
+				HttpResponse response = HttpUtils.doGet(url,queryString);
+				try {
+					return EntityUtils.toString(response.getEntity());
+				} catch (ParseException e) {
+					errorListener.onError(e);
+				} catch (IOException e) {
+					errorListener.onError(e);
+				}
+				return null;
 			}
 
 			@Override
-			public void onComplete(HttpResponse result) {
+			public void onComplete(String result) {
 				listener.onResponse(result);
 				
 			}
 
 			@Override
 			public void onError(Exception error) {
-				// TODO Auto-generated method stub
+				errorListener.onError(error);
 				
 			}
 		});
--- src/edu/vt/wuvt/androidwuvt/view/mainactivity/MainActivity.java
+++ src/edu/vt/wuvt/androidwuvt/view/mainactivity/MainActivity.java
@@ -1,7 +1,15 @@
 package edu.vt.wuvt.androidwuvt.view.mainactivity;
 
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.ParseException;
+import org.apache.http.util.EntityUtils;
+
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -14,12 +22,16 @@ import edu.vt.wuvt.androidwuvt.R;
 import edu.vt.wuvt.androidwuvt.mediaplayer.WuvtMediaPlayer;
 import edu.vt.wuvt.androidwuvt.mediaplayer.WuvtMediaPlayer.WuvtIsPlayingListener;
 import edu.vt.wuvt.androidwuvt.mediaplayer.WuvtMediaPlayer.WuvtPlayerReadyListener;
+import edu.vt.wuvt.androidwuvt.utils.httputils.AsyncHttpUtils;
+import edu.vt.wuvt.androidwuvt.utils.httputils.AsyncHttpUtils.AsyncHttpErrorListener;
+import edu.vt.wuvt.androidwuvt.utils.httputils.AsyncHttpUtils.AsyncHttpListener;
 
 public class MainActivity extends SherlockActivity {
 	
 	private Button play;
 	private Button pause;
 	private Button stop;
+	public static final String TAG = MainActivity.class.getName();
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -69,6 +81,23 @@ public class MainActivity extends SherlockActivity {
 			public void onClick(View v) {
 				wuvtPlayer.stop();
 				wuvtPlayer.isPlaying(playerListener);
+			}
+		});
+		
+		AsyncHttpUtils.doGet("http://www.google.com", new AsyncHttpListener() {
+
+
+			@Override
+			public void onResponse(String response) {
+				Toast.makeText(MainActivity.this, response, Toast.LENGTH_LONG).show();
+				
+			}
+		}, new AsyncHttpErrorListener() {
+			
+			@Override
+			public void onError(Exception exception) {
+				// TODO Auto-generated method stub
+				
 			}
 		});
 	}
