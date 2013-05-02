3f4a4ecdba27eee10dba497548d931dec4af7f18
hickeyadamc <hickeyadamc@gmail.com>
2013-05-01 21:32:00 -0400

    ViewPager and tabs adapter implemented

--- src/edu/vt/wuvt/androidwuvt/view/mainactivity/MainActivity.java
+++ src/edu/vt/wuvt/androidwuvt/view/mainactivity/MainActivity.java
@@ -1,105 +1,61 @@
 package edu.vt.wuvt.androidwuvt.view.mainactivity;
 
-import java.io.IOException;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.ParseException;
-import org.apache.http.util.EntityUtils;
-
-import android.graphics.drawable.Drawable;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.Toast;
-
-import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
 
 import edu.vt.wuvt.androidwuvt.R;
-import edu.vt.wuvt.androidwuvt.mediaplayer.WuvtMediaPlayer;
-import edu.vt.wuvt.androidwuvt.mediaplayer.WuvtMediaPlayer.WuvtIsPlayingListener;
-import edu.vt.wuvt.androidwuvt.mediaplayer.WuvtMediaPlayer.WuvtPlayerReadyListener;
-import edu.vt.wuvt.androidwuvt.utils.httputils.AsyncHttpUtils;
-import edu.vt.wuvt.androidwuvt.utils.httputils.AsyncHttpUtils.AsyncHttpErrorListener;
-import edu.vt.wuvt.androidwuvt.utils.httputils.AsyncHttpUtils.AsyncHttpListener;
+import edu.vt.wuvt.androidwuvt.view.mainactivity.fragments.favorites.FavoritesFragment;
+import edu.vt.wuvt.androidwuvt.view.mainactivity.fragments.nowplaying.NowPlayingFragment;
 
-public class MainActivity extends SherlockActivity {
-	
-	private Button play;
-	private Button pause;
-	private Button stop;
-	public static final String TAG = MainActivity.class.getName();
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		final WuvtIsPlayingListener playerListener = new WuvtIsPlayingListener() {
-			
-			@Override
-			public void isPlaying(boolean playing) {
-				Toast.makeText(MainActivity.this, "Wuvt is playing: " + playing, Toast.LENGTH_SHORT).show();
-				
-			}
-		};
-		final WuvtMediaPlayer wuvtPlayer = WuvtMediaPlayer.get(this);
-		wuvtPlayer.prepare(new WuvtPlayerReadyListener() {
-			
-			@Override
-			public void ready() {
-				ProgressBar pb = (ProgressBar) findViewById(R.id.progressBar1);
-				pb.setVisibility(View.INVISIBLE);
-				wuvtPlayer.isPlaying(playerListener);
-				
-			}
-		});
-		play = (Button) findViewById(R.id.play);
-		play.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View arg0) {
-				wuvtPlayer.play();
-				wuvtPlayer.isPlaying(playerListener);
-			}
-		});
-		pause = (Button) findViewById(R.id.pause);
-		pause.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				wuvtPlayer.pause();
-				wuvtPlayer.isPlaying(playerListener);
-			}
-		});
-		stop = (Button) findViewById(R.id.stop);
-		stop.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				wuvtPlayer.stop();
-				wuvtPlayer.isPlaying(playerListener);
-			}
-		});
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+
+
+public class MainActivity extends SherlockFragmentActivity {
+    ViewPager mViewPager;
+    TabsAdapter mTabsAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        instantiateMemberVariables();
+        setupActionBar();
+        createTabs(savedInstanceState);
+
+    }
+
+    private void createTabs(Bundle savedInstanceState) {
+        mTabsAdapter = new TabsAdapter(this, mViewPager);
+        final com.actionbarsherlock.app.ActionBar bar = getSupportActionBar();
+        mTabsAdapter.addTab(bar.newTab().setText("Simple"),
+                FavoritesFragment.class, null);
+        mTabsAdapter.addTab(bar.newTab().setText("Cursor"),
+                NowPlayingFragment.class, null);
+
+        if (savedInstanceState != null) {
+        	//Use saved instance state to set currentTabe
+//            bar.setSelectedNavigationItem(savedInstanceState.getInt("tab", 0));
+        }
 		
-		AsyncHttpUtils.doGet("http://www.google.com", new AsyncHttpListener() {
-
-
-			@Override
-			public void onResponse(String response) {
-				Toast.makeText(MainActivity.this, response, Toast.LENGTH_LONG).show();
-				
-			}
-		}, new AsyncHttpErrorListener() {
-			
-			@Override
-			public void onError(Exception exception) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
 	}
 
+	private void setupActionBar() {
+        final com.actionbarsherlock.app.ActionBar bar = getSupportActionBar();
+        bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        bar.setDisplayOptions(0, ActionBar.DISPLAY_SHOW_TITLE);
+		
+	}
+
+	private void instantiateMemberVariables() {
+		mViewPager = (ViewPager) findViewById(R.id.viewPager1);
+		
+	}
+
+	@Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("tab", getActionBar().getSelectedNavigationIndex());
+    }
 }
